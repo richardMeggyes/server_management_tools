d8fb5491750f880b0d2624c110b0412fbe57db96
richard.meggyes <richard@fordscorpio.hu>
2018-05-04 14:26:22 +0200

    Deleted while loop and added filter to get only today's failed attempts

--- attack logger.py
+++ attack logger.py
@@ -2,18 +2,20 @@ import time
 
 log_path = "/var/log/auth.log"
 
-while True:
-    failed_attempts = []
-    with open(log_path, "r") as f:
-        lines = f.readlines()
+today = time.strftime('%b %d', time.gmtime(time.time()))
+today = today.replace('0', ' ')
 
-    for line in lines:
-        if "Failed" in line:
+failed_attempts = []
+with open(log_path, "r") as f:
+    lines = f.readlines()
+
+for line in lines:
+    if "Failed password" in line:
+        if today in line:
             failed_attempts.append(line)
             #print(line)
 
-    print(len(failed_attempts))
+print(len(failed_attempts))
 
-    msg = 'Csiga VPS - Faied SSH login attempts this week: {}'.format(len(failed_attempts))
+msg = 'Csiga VPS - Faied SSH login attempts this week: {}'.format(len(failed_attempts))
 
-    time.sleep(86400)
